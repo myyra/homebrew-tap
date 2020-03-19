52a14ee04a26c8985d23e2f4bd3bcbada7c61682
Joonas Myhrberg <joonas@myhrberg.email>
2020-03-20 00:39:20 +0200

    Update hrflow to 0.3.0

--- Formula/hrflow.rb
+++ Formula/hrflow.rb
@@ -1,8 +1,8 @@
 class Hrflow < Formula
   desc "HR Flow CLI"
   homepage "https://github.com/myyra/hrflow"
-  url "https://github.com/myyra/hrflow/archive/v0.2.0.zip"
-  sha256 "04ed644cdf040e983a21746dfb7408d675f4907b4f69f210fc44a775a830d557"
+  url "https://github.com/myyra/hrflow/archive/v0.3.0.zip"
+  sha256 "e6933cad7ba40de48d27e890158cc8b695a581f1743f4893a8c1f91f98d7dc3d"
 
   depends_on "go" => :build
 
